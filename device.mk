6e746e14b668e9934aee01f7c854fa566b8f8eaa
MEMENG872 <113292056+MEMENG872@users.noreply.github.com>
2026-07-10 11:01:01 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Inherit common MediaTek IMS
-$(call inherit-product, vendor/mediatek/ims/ims.mk)
+# $(call inherit-product, vendor/mediatek/ims/ims.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
