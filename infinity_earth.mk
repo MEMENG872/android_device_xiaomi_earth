75d0e4cb46723c38ce5d6eb0600194a8604ed2ee
MEMENG872 <113292056+MEMENG872@users.noreply.github.com>
2026-09-24 13:42:20 +0800

    Update infinity_earth.mk

--- infinity_earth.mk
+++ infinity_earth.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile.
-$(call inherit-product, device/xiaomi/earth/device.mk)
+$(call inherit-product, device/xiaomi/earth/device.mk)    
 
 # Inherit some common Project InfinityX stuff.
 $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
